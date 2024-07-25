4785ece35a6b4465a975260841bcb3cf0eefe881
SabinDangol5 <sabindangol07@gmail.com>
2024-07-25 09:52:57 +0545

    updated by SabinDangl on July 25 , 9:52 am

---
+++ sum-of-two-number.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int firstNum,secondNum,sum;
+	printf("Enter first numder:");
+	scanf("%d",&firstNum);
+	printf("\nEnter second numder:");
+	scanf("%d",&secondNum);
+	sum=firstNum+secondNum;
+	printf("\nSum of two number:%d",sum);
+}
